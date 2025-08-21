eb3d448854ed3be9ca4c10c5ca48fe6ec39e1bc0
Mohit <a1885648@adelaide.edu.au>
2025-08-21 12:25:47 +0930

    Fixed Even.c

--- even.c
+++ even.c
@@ -3,17 +3,19 @@
 #include <unistd.h>
 #include <sys/types.h> 
 #include <signal.h> 
-void hup_handler(int sig){
+void signal_handler(int sig){
+    if (sig == SIGHUP) {
+        printf("\nSIGHUP received!\n");
+    } else if (sig == SIGINT) {
+        printf("\nSIGINT received!\n");
+    }
     printf("\nOuch!\n");
-}
-
-void int_handler(int sig){
     printf("\nYeah!\n");
 }
 
 int main(int arg, char *argv[]) {
-    signal(SIGHUP, hup_handler);
-    signal(SIGINT, int_handler);
+    signal(SIGHUP, signal_handler);
+    signal(SIGINT, signal_handler);
 
     int n = atoi(argv[1]);
 
