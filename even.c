ed8f7b9b0ee709365eb9cfb7dbda2b6b59ce4e0d
Mohit <a1885648@adelaide.edu.au>
2025-08-21 14:25:13 +0930

    Fixed Even.c to work with signal safe functions

--- even.c
+++ even.c
@@ -1,29 +1,34 @@
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>  
-#include <signal.h>
-
-void signal_handler(int sig){
-    if (sig == SIGHUP) {
-        printf("Ouch!");
-    } else if (sig == SIGINT) {
-        printf("Yeah!");
-    }
+#include <unistd.h>
+// Signal handler function to handle signals accordingly
+void signal_handler(int sig) {
+  // If HUP signal received then print "Ouch!" in terminal
+  if (sig == SIGHUP) {
+    write(1, "Ouch!\n", sizeof("Ouch!\n") - 1);
+  }
+  // If INT signal received then print "Yeah!" in terminal
+  if (sig == SIGINT) {
+    write(1, "Yeah!\n", sizeof("Yeah!\n") - 1);
+  }
 }
 
-int main(int arg, char *argv[]) {
-    signal(SIGHUP, signal_handler);
-    signal(SIGINT, signal_handler);
+// Main function tasked with handling signals and printing even numbers
+int main(int argc, char *argv[]) {
 
-    int n = atoi(argv[1]);
-
-    for (int i = 0; i < n; i++) {
-        if (i % 2 == 0) {
-            printf("%d\n", i);
-            sleep(5);
-        }
+  signal(SIGHUP, signal_handler);
+  signal(SIGINT, signal_handler);
+  // Convert command line input to int 
+  int n = atoi(argv[1]);
+  //Check and print first n even numbers
+  for (int i = 0; i < n*2; i +=2) {
+    if (i % 2 == 0) {
+      printf("%d\n", i);
+      // Pause to give program enough time to receive signals
+      sleep(5);
     }
+  }
 
-    return 0;
+  return 0;
 }
-
