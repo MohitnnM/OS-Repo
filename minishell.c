bab49bf238241f30f4188814f76e4398e555696d
Mohit <a1885648@adelaide.edu.au>
2025-08-24 15:42:14 +0930

    k

--- minishell.c
+++ minishell.c
@@ -16,9 +16,15 @@ Compiler/System : gcc/linux
 #define NV 20  /* max number of command tokens */
 #define NL 100 /* input buffer size */
 char line[NL]; /* command input buffer */
-/*
-shell prompt
-*/
+
+struct background_jobs{
+  int job_id;
+  int pid_num;
+  char cmd[NL];
+  int job_status;
+};
+
+/* shell prompt */
 void prompt(void) {
   // ## REMOVE THIS 'fprintf' STATEMENT BEFORE SUBMISSION
   // fprintf(stdout, "\n msh> ");
@@ -29,6 +35,10 @@ void prompt(void) {
 /* envp - environment pointer */
 int main(int argk, char *argv[], char *envp[]) {
   int frkRtnVal;       /* value returned by fork sys call */
+  struct background_jobs bg_jobs[NV]; /* an array to store all background jobs until done */
+  int num_jobs = 0; /* number of background jobs */
+  int stat;
+  int background_tasks; /* number of background tasks*/
   char *v[NV];         /* array of pointers to command line tokens
                         */
   char *sep = " \t\n"; /* command line token separators */
@@ -45,49 +55,77 @@ int main(int argk, char *argv[], char *envp[]) {
     if (line[0] == '#' || line[0] == '\n' || line[0] == '\000') {
       continue; /* to prompt */
     }
+    background_tasks = 0; /* set the background_tasks to 0 again before parsing input */
     v[0] = strtok(line, sep);
     for (i = 1; i < NV; i++) {
       v[i] = strtok(NULL, sep);
       if (v[i] == NULL) {
+        /* when there is argument, then check previous if argument is &, if so then increment background_tasks */
+        if (strcmp(v[i-1],"&") == 0){
+          background_tasks = 1;
+          v[i-1] = NULL; /* remove the & from the argument list */
+        }
         break;
       }
     }
     /* assert i is number of tokens + 1 */
+    
+    /* implement cd to change to current directory */
     if (strcmp(v[0],"cd") == 0)
     {
+      /* error check to see if the location is specified*/
       if(v[1] == NULL){
-        perror("cd: argument is missing");
+        perror("cd: argument missing");
       }
       else{
+        /* run cd is valid otherwise return error if directory is not found*/
         if(chdir(v[1]) != 0){
           perror("cd: directory not found");
         }
       }
+      /* continue to skip fork*/
       continue;
     }
     /* fork a child process to exec the command in v[0] */
     switch (frkRtnVal = fork()) {
       case -1: /* fork returns error to parent process */
       {
-        perror("Error found");
+        perror("fork");
         continue;
       }
       case 0: /* code executed only by child process */
       {
         execvp(v[0], v);
-        perror("Child process");
+        perror("execvp");
         exit(1);
       }
       default: /* code executed only by parent process */
       {
-        int stat;
-        if (wait(&stat) == -1){
-          perror("Parent process wait");
+        if (background_tasks == 0){
+          waitpid(frkRtnVal, &stat, 0);
+          perror("waitpid");
+        }
+        else if (background_tasks == 1){
+          bg_jobs[num_jobs].job_id = num_jobs + 1;
+          bg_jobs[num_jobs].pid_num = frkRtnVal;
+          strcpy(bg_jobs[num_jobs].cmd, v[0]);
+          bg_jobs[num_jobs].job_status = 1;
+          printf("[%d] %d\n", bg_jobs[num_jobs].job_id, frkRtnVal);
+          num_jobs++;
         }
         // REMOVE PRINTF STATEMENT BEFORE SUBMISSION
         // printf("%s done \n", v[0]);
         break;
       }
     } /* switch */
+    pid_t job_finished;
+    while ((job_finished = waitpid(-1, &stat, WNOHANG)) > 0){
+      for(int j = 0; j < num_jobs; ++j){
+        if(bg_jobs[j].pid_num == job_finished){
+          bg_jobs[j].job_status = 0;
+          printf("[%d]+ Done                 %s\n", bg_jobs[j].job_id, bg_jobs[j].cmd);
+        }
+      }
+    }
   } /* while */
 } /* main */
