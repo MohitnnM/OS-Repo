7b9e3382c92b9150e71690965e12989d258e9d53
Mohit <a1885648@adelaide.edu.au>
2025-08-24 16:35:23 +0930

    k

--- minishell.c
+++ minishell.c
@@ -17,7 +17,7 @@ Compiler/System : gcc/linux
 #define NL 100 /* input buffer size */
 char line[NL]; /* command input buffer */
 
-struct background_jobs{
+struct background_jobs {
   int job_id;
   int pid_num;
   char cmd[NL];
@@ -34,15 +34,15 @@ void prompt(void) {
 /* argv - argument vector from command line */
 /* envp - environment pointer */
 int main(int argk, char *argv[], char *envp[]) {
-  int frkRtnVal;       /* value returned by fork sys call */
-  struct background_jobs bg_jobs[NV]; /* an array to store all background jobs until done */
-  int num_jobs = 0; /* number of background jobs */
-  int stat;
+  int frkRtnVal; /* value returned by fork sys call */
+  struct background_jobs
+      bg_jobs[NV];      /* an array to store all background jobs until done */
+  int num_jobs = 0;     /* number of background jobs */
   int background_tasks; /* number of background tasks*/
-  char *v[NV];         /* array of pointers to command line tokens
-                        */
-  char *sep = " \t\n"; /* command line token separators */
-  int i;               /* parse index */
+  char *v[NV];          /* array of pointers to command line tokens
+                         */
+  char *sep = " \t\n";  /* command line token separators */
+  int i;                /* parse index */
   /* prompt for and process one command line at a time */
   while (1) { /* do Forever */
     prompt();
@@ -55,37 +55,38 @@ int main(int argk, char *argv[], char *envp[]) {
     if (line[0] == '#' || line[0] == '\n' || line[0] == '\000') {
       continue; /* to prompt */
     }
-    background_tasks = 0; /* set the background_tasks to 0 again before parsing input */
+    background_tasks =
+        0; /* set the background_tasks to 0 again before parsing input */
     v[0] = strtok(line, sep);
     for (i = 1; i < NV; i++) {
       v[i] = strtok(NULL, sep);
       if (v[i] == NULL) {
-        /* when there is argument, then check previous if argument is &, if so then increment background_tasks */
-        if (strcmp(v[i-1],"&") == 0){
+        /* when there is argument, then check previous if argument is &, if so
+         * then increment background_tasks */
+        if (strcmp(v[i - 1], "&") == 0) {
           background_tasks = 1;
-          v[i-1] = NULL; /* remove the & from the argument list */
+          v[i - 1] = NULL; /* remove the & from the argument list */
         }
         break;
       }
     }
     /* assert i is number of tokens + 1 */
-    
+
     /* implement cd to change to current directory */
-    if (strcmp(v[0],"cd") == 0)
-    {
+    if (strcmp(v[0], "cd") == 0) {
       /* error check to see if the location is specified*/
-      if(v[1] == NULL){
+      if (v[1] == NULL) {
         perror("cd: argument missing");
-      }
-      else{
+      } else {
         /* run cd is valid otherwise return error if directory is not found*/
-        if(chdir(v[1]) != 0){
+        if (chdir(v[1]) != 0) {
           perror("cd: directory not found");
         }
       }
       /* continue to skip fork*/
       continue;
     }
+    int stat;
     /* fork a child process to exec the command in v[0] */
     switch (frkRtnVal = fork()) {
       case -1: /* fork returns error to parent process */
@@ -101,14 +102,18 @@ int main(int argk, char *argv[], char *envp[]) {
       }
       default: /* code executed only by parent process */
       {
-        if (background_tasks == 0){
+        int stat;
+        if (background_tasks == 0) {
           waitpid(frkRtnVal, &stat, 0);
           perror("waitpid");
-        }
-        else if (background_tasks == 1){
+        } else if (background_tasks == 1) {
           bg_jobs[num_jobs].job_id = num_jobs + 1;
           bg_jobs[num_jobs].pid_num = frkRtnVal;
-          strcpy(bg_jobs[num_jobs].cmd, v[0]);
+          bg_jobs[num_jobs].cmd[0] = '\0';  // start with empty string
+          for (int k = 0; v[k] != NULL; ++k) {
+            strcat(bg_jobs[num_jobs].cmd, v[k]);
+            if (v[k + 1] != NULL) strcat(bg_jobs[num_jobs].cmd, " ");
+          }
           bg_jobs[num_jobs].job_status = 1;
           printf("[%d] %d\n", bg_jobs[num_jobs].job_id, frkRtnVal);
           num_jobs++;
@@ -119,11 +124,12 @@ int main(int argk, char *argv[], char *envp[]) {
       }
     } /* switch */
     pid_t job_finished;
-    while ((job_finished = waitpid(-1, &stat, WNOHANG)) > 0){
-      for(int j = 0; j < num_jobs; ++j){
-        if(bg_jobs[j].pid_num == job_finished){
+    while ((job_finished = waitpid(-1, &stat, WNOHANG)) > 0) {
+      for (int j = 0; j < num_jobs; ++j) {
+        if (bg_jobs[j].pid_num == job_finished) {
           bg_jobs[j].job_status = 0;
-          printf("[%d]+ Done                 %s\n", bg_jobs[j].job_id, bg_jobs[j].cmd);
+          printf("[%d]+ Done                 %s\n", bg_jobs[j].job_id,
+                 bg_jobs[j].cmd);
         }
       }
     }
